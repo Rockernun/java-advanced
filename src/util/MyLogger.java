2b144dc775abf3259120dd7ca3dd1cef95231937
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-07-29 11:21:30 +0900

    feat: 현재 시간과 스레드명을 함께 출력하는 MyLogger 유틸 클래스 추가
    - 추상 클래스 MyLogger를 생성하여 외부에서 인스턴스화 방지
    - static log(Object) 메서드를 통해 시각(HH:mm:ss.SSS)과 스레드명을 함께 출력
    - 테스트용 MyLoggerMain 클래스에서 문자열과 숫자 로그 출력 확인

---
+++ src/util/MyLogger.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+// 추상 클래스로 만들어서 직접 생성할 수 없도록 함.
+public abstract class MyLogger {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+
+    public static void log(Object object) {
+        String time = LocalTime.now().format(formatter);
+        System.out.printf("%s [%9s] %s\n", time, Thread.currentThread().getName(), object);
+    }
+}
