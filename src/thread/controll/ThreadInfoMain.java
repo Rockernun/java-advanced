63a7bf34d091d9a5bf2374bcb7513db7e59aae94
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-07-30 14:10:22 +0900

    feat: main 스레드와 새로 생성된 스레드의 상태 및 정보 조회
    - Thread.currentThread()를 통해 main 스레드의 ID, 이름, 우선순위, 그룹, 상태 출력
    - HelloRunnable을 사용해 myThread를 생성하고 동일한 정보 조회
    - myThread가 start() 호출 전이므로 상태가 NEW임을 확인 가능

---
+++ src/thread/controll/ThreadInfoMain.java
@@ -0,0 +1,42 @@
+package thread.controll;
+
+import thread.start.HelloRunnable;
+
+import static util.MyLogger.log;
+
+public class ThreadInfoMain {
+    public static void main(String[] args) {
+
+        Thread mainThread = Thread.currentThread();
+        log("mainThread = " + mainThread);
+        log("mainThread.threadId()=" + mainThread.threadId());
+        log("mainThread.getName()=" + mainThread.getName());
+        log("mainThread.getPriority()=" + mainThread.getPriority());
+        log("mainThread.getThreadGroup()=" + mainThread.getThreadGroup());
+        log("mainThread.getState()=" + mainThread.getState());
+
+        Thread myThread = new Thread(new HelloRunnable(), "myThread");
+        log("myThread = " + myThread);
+        log("myThread.threadId()=" + myThread.threadId());
+        log("myThread.getName()=" + myThread.getName());
+        log("myThread.getPriority()=" + myThread.getPriority());
+        log("myThread.getThreadGroup()=" + myThread.getThreadGroup());
+        log("myThread.getState()=" + myThread.getState());
+    }
+}
+
+/*
+13:44:41.921 [     main] mainThread = Thread[#1,main,5,main]
+13:44:41.925 [     main] mainThread.threadId()=1
+13:44:41.925 [     main] mainThread.getName()=main
+13:44:41.928 [     main] mainThread.getPriority()=5
+13:44:41.928 [     main] mainThread.getThreadGroup()=java.lang.ThreadGroup[name=main,maxpri=10]
+13:44:41.928 [     main] mainThread.getState()=RUNNABLE
+
+13:44:41.929 [     main] myThread = Thread[#21,myThread,5,main]
+13:44:41.929 [     main] myThread.threadId()=21
+13:44:41.929 [     main] myThread.getName()=myThread
+13:44:41.929 [     main] myThread.getPriority()=5
+13:44:41.929 [     main] myThread.getThreadGroup()=java.lang.ThreadGroup[name=main,maxpri=10]
+13:44:41.929 [     main] myThread.getState()=NEW
+*/
