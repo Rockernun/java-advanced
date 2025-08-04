f7961edb6b463e9c47d2264a91f14957f1595f2c
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-08-04 18:52:07 +0900

    refactor: 익명 클래스를 사용하여 불필요한 구현 클래스 제거

---
+++ src/lambda/start/Ex1RefMainV2.java
@@ -0,0 +1,48 @@
+package lambda.start;
+
+import lambda.Procedure;
+
+import java.util.Random;
+
+public class Ex1RefMainV2 {
+
+    public static void hello(Procedure procedure) {
+
+        long startNs = System.nanoTime();
+        procedure.run();
+        long endNs = System.nanoTime();
+        System.out.println("실행 시간: " + (endNs - startNs) + "ns");
+    }
+
+    public static void main(String[] args) {
+
+        Procedure dice = new Procedure() {
+            @Override
+            public void run() {
+                int randomValue = new Random().nextInt(6) + 1;
+                System.out.println("주사위의 눈: " + randomValue);
+            }
+        };
+
+        Procedure sum = new Procedure() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 3; i++) {
+                    System.out.println("i = " + i);
+                }
+            }
+        };
+
+        hello(dice);
+        hello(sum);
+    }
+}
+
+/*
+주사위의 눈: 1
+실행 시간: 3125167ns
+i = 1
+i = 2
+i = 3
+실행 시간: 136250ns
+*/
