bdc0324c7052250f701194fe33b09122f23b4bb8
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-10-20 12:27:26 +0900

    feat(lambda): 문자열 매핑 기본 구현
    - 예제 리스트 ["1","12","123","1234"] 추가
    - mapStringToInteger(): 문자열 → 정수 변환 로직
    - mapStringToLength(): 문자열 길이 계산 로직

---
+++ src/lambda/lambda5/map/MapMainV1.java
@@ -0,0 +1,37 @@
+package lambda.lambda5.map;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapMainV1 {
+    public static void main(String[] args) {
+
+        List<String> list = List.of("1", "12", "123", "1234");
+
+        // 문자열을 숫자로 변환
+        List<Integer> numbers = mapStringToInteger(list);
+        System.out.println("numbers = " + numbers);
+
+        // 문자열의 길이를 반환
+        List<Integer> length = mapStringToLength(list);
+        System.out.println("length = " + length);
+    }
+
+    static List<Integer> mapStringToInteger(List<String> list) {
+        List<Integer> result = new ArrayList<>();
+        for (String s : list) {
+            result.add(Integer.parseInt(s));
+        }
+
+        return result;
+    }
+
+    static List<Integer> mapStringToLength(List<String> list) {
+        List<Integer> result = new ArrayList<>();
+        for (String s : list) {
+            result.add(s.length());
+        }
+
+        return result;
+    }
+}
